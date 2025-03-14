0dba49629547f229f76f4ca8a679f4c5fb3d9197
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-03-14 13:22:13 +0600

    Make the footer

--- Agro-Mart-Client/src/layout/MainLayout.jsx
+++ Agro-Mart-Client/src/layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <div className="max-w-7xl mx-auto space-y-5 px-5">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
