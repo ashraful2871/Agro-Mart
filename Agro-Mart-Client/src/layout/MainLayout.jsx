72c23b0abb94b5689de25c45a5f9f0ff1d7731e2
ashraful2871 <ashrafulislam2758@gmail.com>
2025-03-17 21:47:07 +0600

    implement signOut

--- Agro-Mart-Client/src/layout/MainLayout.jsx
+++ Agro-Mart-Client/src/layout/MainLayout.jsx
@@ -8,7 +8,7 @@ const MainLayout = () => {
     <>
       <div className="max-w-7xl mx-auto space-y-5 px-5">
         <Navbar></Navbar>
-        <div className="min-h-screen">
+        <div className="min-h-[500px] container mx-auto p-3 md:px-2 lg:px-0">
           <Outlet></Outlet>
         </div>
       </div>
