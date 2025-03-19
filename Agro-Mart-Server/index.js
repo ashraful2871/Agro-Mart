639cbbb6ee24fdf5aac25f8ab87e7b29c5357d25
aaliahammedpriom <aaliahammedpriom66@gmail.com>
2025-03-19 11:45:08 +0600

    reorder sequence

--- Agro-Mart-Server/index.js
+++ Agro-Mart-Server/index.js
@@ -10,9 +10,8 @@ app.use(express.json());
 //agro1234
 //tkWMj4u0as7kNvYI
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sotib.mongodb.net/?appName=Cluster0`;
-console.log(uri)
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,46 +25,65 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-    const database = client.db("AgroMart")
-    const productCollection = database.collection("products")
-
-    // Product management
-  app.post('/products', async (req, res) => {
-      const { name, category, price, description, stockQuantity, imageURL, addedBy } = req.body;
-  
-      const productData = {
-          name,
-          category,
-          price,
-          description,
-          stockQuantity,
-          imageURL,
-          addedBy,
-      };
-  
-      try {
-          const result = await productCollection.insertOne(productData);
-          res.send(result); 
-      } catch (error) {
-          res.status(500).send({ message: "Error inserting the product." });
-          console.error(error);
-      }
-  });
-
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    const usersCollection = client.db("agroMart").collection("users")
-    const productsCollection = client.db("agroMart").collection("products")
 
-    app.post('/products', (req, res) => {
-      const result = productsCollection.insertOne(req.body)
+    const usersCollection = client.db("AgroMart").collection("users")
+    const productCollection = client.db("AgroMart").collection("products")
+
+
+    //users related apis
+    app.post('/users', (req, res) => {
+      const result = usersCollection.insertOne(req.body)
       req.send(result)
     })
+    app.get('/users', (req, res) => {
+      const result = usersCollection.find().toArray()
+      req.send(result)
+    })
+    app.get('/users/:uid', (req, res) => {
+      const result = usersCollection.findOne()
+      req.send(result)
+    })
+
+
+
+    // products related apis crud
+
+    // products create
+    app.post('/products', async (req, res) => {
+      const { name, category, price, description, stockQuantity, imageURL, addedBy } = req.body;
+
+      const productData = {
+        name,
+        category,
+        price,
+        description,
+        stockQuantity,
+        imageURL,
+        addedBy,
+      };
+
+      try {
+        const result = await productCollection.insertOne(productData);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Error inserting the product." });
+        console.error(error);
+      }
+    });
+
+    // products get
     app.get('/products', (req, res) => {
       const result = productsCollection.find().toArray()
+      req.send(result)
+    })
+    // products get by _id
+    app.get('/products/:id', (req, res) => {
+      const query = {_id : new ObjectId (req.params.id)}
+      const result = productsCollection.findOne(query)
       req.send(result)
     })
 
