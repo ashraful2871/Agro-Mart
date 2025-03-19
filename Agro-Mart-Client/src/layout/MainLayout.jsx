a3365b11733329fda6a2ac883c4cfc27845b7450
ashraful2871 <ashrafulislam2758@gmail.com>
2025-03-19 12:24:41 +0600

    change some marging paddinng and alignment

--- Agro-Mart-Client/src/layout/MainLayout.jsx
+++ Agro-Mart-Client/src/layout/MainLayout.jsx
@@ -7,14 +7,19 @@ const MainLayout = () => {
   const location = useLocation();
 
   const isLoginPage = location.pathname === "/login";
+  const isHomePage = location.pathname === "/";
   const isSignUpPage = location.pathname === "/register";
   return (
     <>
-      <div>
+      <div
+        className={`${
+          !isHomePage ? "max-w-7xl mx-auto md:space-y-5 md:p-5 xl:p-0" : ""
+        }`}
+      >
         <div className={`${isLoginPage || isSignUpPage ? "hidden" : ""}`}>
           <Navbar></Navbar>
         </div>
-        <div className="min-h-[500px]  p-3 md:px-2 lg:px-0">
+        <div className="min-h-[814px]  p-3 md:px-2 lg:px-0">
           <Outlet></Outlet>
         </div>
       </div>
