2c41c9277e310a6abadec6b788abbfcb0133763f
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-03-21 03:41:27 +0600

    add some about sections

---
+++ Agro-Mart-Client/src/pages/About/AboutsSections/Logos.jsx
@@ -0,0 +1,19 @@
+import "./Logos.css";
+
+const Logos = () => {
+    return (
+        <div class="marquee-container">
+          <div class="marquee-content">
+            <div><img src="https://i.ibb.co.com/fYTFvRjF/food.png" alt="" /></div>
+            <div><img src="https://i.ibb.co.com/mrRcyD0G/fruit.png" alt="" /></div>
+            <div><img src="https://i.ibb.co.com/FkP2QZh8/landscape.png" alt="" /></div>
+            <div><img src="https://i.ibb.co.com/5xW7ptr5/food-1.png" alt="" /></div>
+            <div><img src="https://i.ibb.co.com/p6cCgRdC/silhouette.png" alt="" /></div>
+            <div><img src="https://i.ibb.co.com/Ng8TCgLd/silhouette-1.png" alt="" /></div>
+            <div><img src="https://i.ibb.co.com/RG1YFRpT/technology.png" alt="" /></div>
+          </div>
+        </div>
+    );
+};
+
+export default Logos;
