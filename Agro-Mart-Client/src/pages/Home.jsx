ed96fb4fff66c8f7ea78dbf29a3746359c53f9bc
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-03-23 03:19:58 +0600

    added: Parallax section and why user choose us section in home page & modify: review section

--- Agro-Mart-Client/src/pages/Home.jsx
+++ Agro-Mart-Client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Slider from "../components/slider/Slider";
 import FaqSection from "../components/FaqSection/FaqSection";
 import ReviewPosting from "../components/ReviewPosting/ReviewPosting";
 import ReviewDisplay from "../components/ReviewDisplay/ReviewDisplay";
+import WhyChooseUs from "./Home/WhyChooseUs/WhyChooseUs";
+import ParallaxSection from "./Home/ParallaxSection/ParallaxSection";
 
 const Home = () => {
   return (
@@ -13,10 +15,20 @@ const Home = () => {
       <section className="max-w-screen-xl mx-auto mt-5 p-2">
         <FaqSection></FaqSection>
       </section>
+
       {/* Review Posting Section */}
-      <section>
+      {/* <section>
         <ReviewPosting></ReviewPosting>
+      </section> */}
+
+      {/* Why Choose Us */}
+      <section>
+        <WhyChooseUs></WhyChooseUs>
       </section>
+
+      {/* Parallax Section */}
+      <ParallaxSection></ParallaxSection>
+
       {/* Review Display */}
       <section>
         <ReviewDisplay></ReviewDisplay>
