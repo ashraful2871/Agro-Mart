7195a3e2c2ef8646014ba6c56beee22ce5850a49
ashraful2871 <ashrafulislam2758@gmail.com>
2025-03-22 22:23:18 +0600

    added new dashboard

---
+++ Agro-Mart-Client/src/dashboard/admin-menu/AdminMenu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const AdminMenu = () => {
+  return (
+    <div>
+      <ul className="menu font-semibold text-base text-base-content">
+        {" "}
+        <li>
+          <NavLink to="/dashboard/over-view">Overview</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dashboard/all-users">Customers</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dashboard/all-materials">Orders</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AdminMenu;
