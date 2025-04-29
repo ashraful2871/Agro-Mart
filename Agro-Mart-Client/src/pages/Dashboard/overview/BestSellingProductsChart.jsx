657b0295a862218f3941c3fddc99bcd71041b074
ashraful2871 <ashrafulislam2758@gmail.com>
2025-04-29 19:20:22 +0600

    MODIFY: translet line chart and best selling product in admin dashboard

--- Agro-Mart-Client/src/pages/Dashboard/overview/BestSellingProductsChart.jsx
+++ Agro-Mart-Client/src/pages/Dashboard/overview/BestSellingProductsChart.jsx
@@ -1,17 +1,28 @@
 import React, { useContext, useEffect, useState } from "react";
 import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from "recharts";
 import { ThemeContext } from "../../../provider/ThemeProvider";
+import { useTranslation } from "react-i18next";
 
-const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#8dd1e1", "#d0ed57"];
+const COLORS = [
+  "#8884d8",
+  "#82ca9d",
+  "#ffc658",
+  "#ff8042",
+  "#8dd1e1",
+  "#d0ed57",
+];
 
 const BestSellingProductsChart = () => {
+  const { t } = useTranslation();
   const { theme } = useContext(ThemeContext);
   const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/best-selling-products`);
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/best-selling-products`
+        );
         const result = await res.json();
         console.log("Fetched Result:", result);
 
@@ -35,8 +46,14 @@ const BestSellingProductsChart = () => {
   }, []);
 
   return (
-    <div className={`${theme === "dark" ? "bg-[#1F2937]" : "bg-white"} p-4 md:p-6 rounded-lg shadow-md w-full h-full`}>
-      <h2 className="text-lg font-bold mb-4">Best Selling Products</h2>
+    <div
+      className={`${
+        theme === "dark" ? "bg-[#1F2937]" : "bg-white"
+      } p-4 md:p-6 rounded-lg shadow-md w-full h-full`}
+    >
+      <h2 className="text-lg font-bold mb-4">
+        {t("dashboard.best_selling_products_chart.title")}
+      </h2>
       <div className="w-full">
         {chartData.length > 0 ? (
           <ResponsiveContainer width="100%" height={300}>
@@ -50,7 +67,9 @@ const BestSellingProductsChart = () => {
                 outerRadius="80%"
                 innerRadius="60%"
                 paddingAngle={2}
-                label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+                label={({ name, percent }) =>
+                  `${name}: ${(percent * 100).toFixed(0)}%`
+                }
                 labelLine={false}
               >
                 {chartData.map((entry, index) => (
@@ -76,7 +95,9 @@ const BestSellingProductsChart = () => {
             </PieChart>
           </ResponsiveContainer>
         ) : (
-          <p className="text-center text-gray-500">No data available</p>
+          <p className="text-center text-gray-500">
+            {t("dashboard.best_selling_products_chart.no_data")}
+          </p>
         )}
       </div>
     </div>
